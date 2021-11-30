83d006aba60f02ff3690c6bcc5032a1e0f953df8
airan-rocha <rocha.airan@gmail.com>
2021-11-29 22:30:31 -0300

    whatsapp editado

--- itens/redeSocial.js
+++ itens/redeSocial.js
@@ -6,7 +6,7 @@ redeSocial.forEach(element => {
     const {name, url, logoUrl} = element;
 
     redesSociais += (`
-            <a target='_blank' href='${url}'><img alt='logo ${name}' src='${logoUrl}'></a>
+            <a href='${url}'><img alt='logo ${name}' src='${logoUrl}'></a>
     `);
 });
 
